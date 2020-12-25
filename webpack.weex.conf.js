06430cce7959b1735f87f998873fb652f8b65647
liangzw7 <1901543053@qq.com>
2020-12-25 10:39:18 +0800

    解决production banner 问题

--- webpack.weex.conf.js
+++ webpack.weex.conf.js
@@ -1,12 +1,52 @@
-const path = require('path')
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname,'./src/main.js'),
-    output: {
-        path: path.resolve('./dist'),
-        filename: '[name].[hash:6].js'
-    },
-    module: {},
-    plugins: []
-}
+	mode: 'production',
+	watch: true,
+	entry: path.resolve(__dirname, './src/main.js'),
+	output: {
+		path: path.resolve('./dist'),
+		filename: '[name].[hash:6].js',
+	},
+	resolve: {
+		extensions: ['.js', '.vue', '.json'],
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	module: {},
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.BannerPlugin({
+		    banner: '// { "framework": "Vue"} \n',
+		    raw: true,
+		})],
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: false,
+				// extractComments: {
+				//   banner: '// { "framework": "Vue" } \n'
+				// },
+				terserOptions: {
+					mangle: true,
+					compress: {
+						unused: true,
+						warnings: false,
+						drop_debugger: true,
+					},
+					output: {
+						comments: /^\**!|\"framework\"/i,
+					},
+				},
+			}),
+		],
+	},
+};
